d9a1b8d1e3801a6e088fdf47214b41f113d8174c
sangh0 <hop7311@naver.com>
2022-07-30 23:14:02 +0900

    update custom dataset

---
+++ PIDNet/datasets/camvid.py
@@ -0,0 +1,87 @@
+import numpy as np
+from PIL import Image
+from glob import glob
+
+import torch
+from torch.utils.data import Dataset
+import torchvision.transforms as transforms
+
+from ..util.transform import Compose, RandomCrop, HorizontalFlip, RandomScale, ColorJitter
+
+"""
+path : cityscapes/
+├── train
+│    ├─ train_images
+│       ├─ image1.jpg
+│       ├─ ...
+│    ├─ train_labels
+│       ├─ label1.jpg 
+│       ├─ ...
+├── valid
+│    ├─ valid_images
+│       ├─ image1.jpg
+│       ├─ ...
+│    ├─ valid_labels
+│       ├─ label1.jpg
+│       ├─ ... 
+├── test
+│    ├─ test_images
+│       ├─ image1.jpg
+│       ├─ ...
+│    ├─ test_labels
+│       ├─ label1.jpg
+│       ├─ ... 
+"""
+
+"""
+Format of label: 3 RGB channels with segmentation map
+"""
+
+class CamVidDataset(Dataset):
+    def __init__(
+        self,
+        path,
+        subset,
+        cropsize=None,
+        ignore_index=255,
+    ):
+        assert subset in ('train', 'valid', 'test')
+        self.image_files = glob(path+'/'+subset+'/'+subset+'_images/*.png')
+        self.color_files = glob(path+'/'+subset+'/'+subset+'_labels/*.png')
+        self.subset = subset
+        self.ignore_index = ignore_index
+        
+        self.totensor = transforms.Compose([
+            transforms.ToTensor(),
+        ])
+        self.augment = Compose([
+            HorizontalFlip(),
+            RandomScale((0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0)),
+            RandomCrop(cropsize)
+        ])
+        self.color_list = [[0,128,192], [128,0,0], [64,0,128], 
+                           [192,192,128], [64,64,128], [64,64,0],
+                           [128,64,128], [0,0,192], [192,128,128],
+                           [128,128,128], [128,128,0]]
+        
+    def __len__(self):
+        return len(self.image_files)
+    
+    def __getitem__(self, idx):
+        images = Image.open(self.image_files[idx]).convert('RGB')
+        colors = Image.open(self.color_files[idx]).convert('RGB')
+        labels = self.color2label(colors)
+        if self.subset == 'train':
+            im_lb = dict(im=images, lb=Image.fromarray(labels))
+            im_lb = self.augment(im_lb)
+            images, labels = im_lb['im'], im_lb['lb']
+        labels = np.array(labels)[np.newaxis,:]
+            
+        return self.totensor(images), torch.LongTensor(labels)
+            
+    def color2label(self, color_map):
+        color_map = np.array(color_map)
+        label = np.ones(color_map.shape[:2]) * self.ignore_index
+        for i, color in enumerate(self.color_list):
+            label[(color_map==color).sum(2)==3] = i
+        return label
---
+++ PIDNet/datasets/cityscapes.py
@@ -0,0 +1,95 @@
+import numpy as np
+from PIL import Image
+from glob import glob
+
+import torch
+from torch.utils.data import Dataset
+import torchvision.transforms as transforms
+
+from ..util.transform import Compose, RandomCrop, HorizontalFlip, RandomScale, ColorJitter
+
+"""
+path : cityscapes/
+├── images
+│    ├─ train
+│       ├─ image1.jpg
+│       ├─ ...
+│    ├─ valid
+│       ├─ image1.jpg 
+│       ├─ ...
+│    ├─ test
+│       ├─ image1.jpg 
+│       ├─ ...
+├── labels
+│    ├─ train
+│       ├─ label1.jpg
+│       ├─ ...
+│    ├─ valid
+│       ├─ label1.jpg
+│       ├─ ... 
+"""
+
+"""
+Format of label: single channel with one-hot encoding
+"""
+
+class CityscapesDataset(Dataset):
+    def __init__(
+        self,
+        path,
+        subset,
+        cropsize=None,
+        ignore_index=255,
+    ):
+        assert subset in ('train', 'valid', 'test')
+        self.image_files = glob(path+'/images/'+subset+'/**/*.png')
+        if subset not in 'test':
+            self.label_files = [
+                file for file in glob(path+'/labels/'+subset+'/**/*.png') \
+                if 'gtFine_labelIds' in file
+            ]
+        self.subset = subset
+        self.cropsize = cropsize
+        self.ignore_index = ignore_index
+        self.totensor = transforms.Compose([
+            transforms.ToTensor(),
+#             transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+        ])
+        self.augment = Compose([
+            HorizontalFlip(),
+            RandomScale((0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0)),
+            RandomCrop(cropsize)
+        ])
+        self.mapping_20 = {
+            0: ignore_index, 1: ignore_index, 2: ignore_index, 3: ignore_index, 
+            4: ignore_index, 5: ignore_index, 6: ignore_index, 7: 0, 8: 1, 
+            9: ignore_index, 10: ignore_index, 11: 2, 12: 3, 13: 4, 
+            14: ignore_index, 15: ignore_index, 16: ignore_index, 17: 5, 
+            18: ignore_index, 19: 6, 20: 7, 21: 8, 22: 9, 23: 10, 24: 11, 
+            25: 12, 26: 13, 27: 14, 28: 15, 29: ignore_index, 30: ignore_index, 
+            31: 16, 32: 17, 33: 18, -1: ignore_index
+        }
+        
+    def __len__(self):
+        return len(self.image_files)
+    
+    def __getitem__(self, idx):
+        images = Image.open(self.image_files[idx]).convert('RGB')
+        if self.subset=='train':
+            labels = Image.open(self.label_files[idx]).convert('L')
+            im_lb = dict(im=images, lb=labels)
+            im_lb = self.augment(im_lb)
+            images, labels = im_lb['im'], im_lb['lb']
+            labels = np.array(labels)[np.newaxis,:]
+            return self.totensor(images), self.convert_label(labels)
+        elif self.subset=='valid':
+            labels = Image.open(self.label_files[idx]).convert('L')
+            labels = np.array(labels)[np.newaxis,:]
+            return self.totensor(images), self.convert_label(labels)
+        else:
+            return self.totensor(images)
+    
+    def convert_label(self, label):
+        for k in self.mapping_20:
+            label[label==k] = self.mapping_20[k]
+        return torch.LongTensor(label)

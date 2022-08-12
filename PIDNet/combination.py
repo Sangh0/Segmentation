ab16e449d8dabe72286fdee59e6e0fa10078f8ec
sangh0 <hop7311@naver.com>
2022-08-13 00:59:17 +0900

    update combination code to combine model and loss functions

--- PIDNet/combination.py
+++ PIDNet/combination.py
@@ -10,8 +10,7 @@ class Combination(nn.Module):
         sem_loss,
         bd_loss,
         metrics,
-        ignore_index=255,
-        
+        ignore_index=255, 
     ):
         super(Combination, self).__init__()
         self.model = model
@@ -41,18 +40,18 @@ class Combination(nn.Module):
                     align_corners=False,
                 )
                 
-        pix_acc = self.get_pixel_acc(outputs[1], labels)
-        mean_iou = self.get_mean_iou(outputs[1], labels)
-        semantic_loss = self.sem_loss(outputs[:2], labels)
-        boundary_loss = self.bd_loss(outputs[2], edges)
+        pix_acc = self.cal_pixel_acc(outputs[1], labels)
+        mean_iou = self.cal_mean_iou(outputs[1], labels)
+        semantic_loss = self.sem_loss(outputs[:2], labels.squeeze(dim=1))
+        boundary_loss = self.bd_loss(outputs[2], edges.float())
         
-        filler = torch.ones_like(labels) * self.ignore_index
-        bd_labels = torch.where(F.sigmoid(outputs[2][:,0,:,:])>0.8, labels, filler)
+        filler = torch.ones_like(labels.squeeze(dim=1)) * self.ignore_index
+        bd_labels = torch.where(torch.sigmoid(outputs[2][:,0,:,:])>0.8, labels.squeeze(dim=1), filler)
         sb_loss = self.sem_loss(outputs[1], bd_labels)
         total_loss = semantic_loss + boundary_loss + sb_loss
         
         return {
-            'total_loss': total_loss,
+            'total_loss': torch.unsqueeze(total_loss, dim=0),
             'boundary_loss': boundary_loss,
             'semantic_loss': semantic_loss,
             'pixel_accuracy': pix_acc,

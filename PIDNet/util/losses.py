4fa89f925ef56a56ffd4912b0b68ae5bc1885ea6
sangh0 <hop7311@naver.com>
2022-08-12 23:12:51 +0900

    update loss functions

--- PIDNet/util/losses.py
+++ PIDNet/util/losses.py
@@ -1,42 +1,107 @@
 import numpy as np
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class CELoss(nn.Module):
+
+class CrossEntropyLoss(nn.Module):
     
-    def __init__(self, ignore_index=255, weight=None):
-        super(CELoss, self).__init__()
-        self.ignore_index = ignore_index
+    def __init__(
+        self, 
+        ignore_label=-1, 
+        weight=None,
+        balance_weights=[0.5, 0.5],
+        sb_weights=0.5,
+    ):
+        super(CrossEntropyLoss, self).__init__()
+        self.ignore_label = ignore_label
         self.criterion = nn.CrossEntropyLoss(
             weight=weight,
-            ignore_index=ignore_index,
+            ignore_index=ignore_label,
         )
+        self.balance_weights = balance_weights
+        self.sb_weights = sb_weights
 
-    def forward(self, pred, label):
-        return self.criterion(pred, label)
+    def _forward(self, score, target):
+        loss = self.criterion(score, target)
+        return loss
+
+    def forward(self, score, target):
+
+        if len(score) == 1:
+            score = [score]
+            
+        if len(self.balance_weights) == len(score):
+            return sum([w * self._forward(x, target) for (w, x) in zip(self.balance_weights, score)])
+        elif len(score) == 1:
+            return self.sb_weights * self._forward(score[0], target)
+        
+        else:
+            raise ValueError("lengths of prediction and target are not identical!")
         
 
 class OhemCELoss(nn.Module):
-
-    def __init__(self, thresh=0.7, ignore_index=255, weight=None):
+    
+    def __init__(
+        self, 
+        ignore_label=255, 
+        thres=0.7,
+        min_kept=100000, 
+        weight=None,
+        balance_weights=[0.5, 0.5],
+        sb_weights=0.5,
+    ):
         super(OhemCELoss, self).__init__()
-        self.thresh = -torch.log(torch.tensor(thresh, requires_grad=False, dtype=torch.float)).cuda()
-        self.ignore_lb = ignore_index
-        self.criteria = nn.CrossEntropyLoss(
-            weight=weight, 
-            ignore_index=ignore_index, 
+        self.thresh = thres
+        self.min_kept = max(1, min_kept)
+        self.ignore_label = ignore_label
+        self.criterion = nn.CrossEntropyLoss(
+            weight=weight,
+            ignore_index=ignore_label,
             reduction='none',
         )
+        self.balance_weights = balance_weights
+        self.sb_weights = sb_weights
 
-    def forward(self, logits, labels):
-        n_min = labels[labels != self.ignore_lb].numel() // 16
-        loss = self.criteria(logits, labels).view(-1)
-        loss_hard = loss[loss > self.thresh]
-        if loss_hard.numel() < n_min:
-            loss_hard, _ = loss.topk(n_min)
-        return torch.mean(loss_hard)
+    def _ce_forward(self, score, target):
+        loss = self.criterion(score, target)
+        return loss
 
+    def _ohem_forward(self, score, target, **kwargs):
+        pred = F.softmax(score, dim=1)
+        pixel_losses = self.criterion(score, target).contiguous().view(-1)
+        mask = target.contiguous().view(-1) != self.ignore_label
+
+        tmp_target = target.clone()
+        tmp_target[tmp_target == self.ignore_label] = 0
+        pred = pred.gather(1, tmp_target.unsqueeze(1))
+        pred, ind = pred.contiguous().view(-1,)[mask].contiguous().sort()
+        min_value = pred[min(self.min_kept, pred.numel() - 1)]
+        threshold = max(min_value, self.thresh)
+
+        pixel_losses = pixel_losses[mask][ind]
+        pixel_losses = pixel_losses[pred < threshold]
+        return pixel_losses.mean()
+
+    def forward(self, score, target):
+        if not (isinstance(score, list) or isinstance(score, tuple)):
+            score = [score]
+
+        if len(self.balance_weights) == len(score):
+            functions = [self._ce_forward] * \
+                (len(self.balance_weights) - 1) + [self._ohem_forward]
+            return sum([
+                w * func(x, target)
+                for (w, x, func) in zip(self.balance_weights, score, functions)
+            ])
+        
+        elif len(score) == 1:
+            return self.sb_weights * self._ohem_forward(score[0], target)
+        
+        else:
+            raise ValueError("lengths of prediction and target are not identical!")
+        
 
 class BoundaryLoss(nn.Module):
     
@@ -44,7 +109,7 @@ class BoundaryLoss(nn.Module):
         super(BoundaryLoss, self).__init__()
         self.coeff_bce = coeff_bce
 
-    def weighted_bce(self, pred, label):
+    def weighted_bce(self, score, target):
         N, C, H, W = pred.size()
         pred = pred.permute(0,2,3,1).contiguous().view(1, -1)
         label = label.view(1, -1)

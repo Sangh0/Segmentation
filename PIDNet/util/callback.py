85787b8a903dec0474446c7701388ec7fbb53eeb
sangh0 <hop7311@naver.com>
2022-08-12 23:11:52 +0900

    modify check point module

--- PIDNet/util/callback.py
+++ PIDNet/util/callback.py
@@ -10,33 +10,32 @@ model check point and early stopping modules
 # We set check point to save model for getting best validation loss
 # this module is stored when the validation loss decreases for each epoch
 # and, if validation loss does not decrease in the next epoch in training, do not store it 
-class CheckPoint:
-    def __init__(self, verbose=False, path='checkpoint.pt', trace_func=print):
+class CheckPoint(object):
+    def __init__(self, verbose=False, trace_func=print):
         self.verbose = verbose
-        self.path = path
         self.trace_func = trace_func
         self.val_loss_min = np.Inf
         self.best_score = None
 
-    def __call__(self, val_loss, model):
+    def __call__(self, val_loss, model, path):
         score = -val_loss
         if self.best_score is None:
             self.best_score = score
-            self.save_checkpoint(val_loss, model)
+            self.save_checkpoint(val_loss, model, path)
         elif score > self.best_score:
             self.best_score = score
-            self.save_checkpoint(val_loss, model)
+            self.save_checkpoint(val_loss, model, path)
 
-    def save_checkpoint(self, val_loss, model):
+    def save_checkpoint(self, val_loss, model, path):
         if self.verbose:
             self.trace_func(f'Validation loss decreased ({self.val_loss_min:.3f} --> {val_loss:.3f}).  Saving model ...')
-        torch.save(model.state_dict(), self.path)
+        torch.save(model.state_dict(), path)
         self.val_loss_min = val_loss
 
 
 # We set early stopping to check over fitting of model
 # this module can be perform above checkpoint with early stopping
-class EarlyStopping:
+class EarlyStopping(object):
     def __init__(self, patience=7, verbose=False, delta=0, path='es_checkpoint.pt', trace_func=print):
         self.patience = patience
         self.verbose = verbose

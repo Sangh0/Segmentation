c0c0008e7e2ef4f687b3be08dc89c2c009c08795
Sangh0 <hop7311@naver.com>
2023-01-15 01:14:14 +0900

    update model.py implemented PyTorch

--- FCN/model.py
+++ FCN/model.py
@@ -1,78 +1,135 @@
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras.models import Model, Sequential
-from tensorflow.keras.layers import (
-    Conv2D,
-    MaxPooling2D,
-    Conv2DTranspose,
-    concatenate,
-    BatchNormalization,
-    UpSampling2D,
-)
-from tensorflow.keras.applications.vgg16 import VGG16
+from typing import *
 
-# build encoder
-def encoder(img_size):
-    width, height = img_size
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision.models import vgg16_bn, VGG16_BN_Weights
+
+
+class Encoder(nn.Module):
     
-    vgg = VGG16(
-        weights='imagenet',
-        include_top=False,
-        input_shape=(width, height, 3),
-    )
+    def __init__(self):
+        super(Encoder, self).__init__()
+        vgg = vgg16_bn(weights=VGG16_BN_Weights)
+        
+        self.block1 = self._make_block_module_(vgg, [0, 7])
+        self.block2 = self._make_block_module_(vgg, [7, 14])
+        self.block3 = self._make_block_module_(vgg, [14, 24])
+        self.block4 = self._make_block_module_(vgg, [24, 34])
+        self.block5 = self._make_block_module_(vgg, [34, 44])
+        self.block6 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=1, stride=1, padding=0),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=1, stride=1, padding=0),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+        )
 
-    pool3 = vgg.get_layer('block3_pool').output
-    pool4 = vgg.get_layer('block4_pool').output
-    pool5 = vgg.get_layer('block5_pool').output
+    def _make_block_module_(self, vgg: nn.Module, indices: List[int, int]):
+        layers = []
+        for i in range(indices[0], indices[1]):
+            layers.append(self.vgg.features[i])
 
-    conv6 = Conv2D(512, kernel_size=3, strides=1, padding='same', activation='relu')(pool5)
-    conv6 = BatchNormalization()(conv6)
-    conv7 = Conv2D(512, kernel_size=3, strides=1, padding='same', activation='relu')(conv6)
-    conv7 = BatchNormalization()(conv7)
+        return nn.Sequential(*layers)
 
-    encoder_model = Model(inputs=[vgg.input], outputs=[conv7])
+    def forward(self, x):
+        x1 = self.block1(x)
+        x2 = self.block2(x1)
+        x3 = self.block3(x2)
+        x4 = self.block4(x3)
+        x5 = self.block5(x4)
+        x6 = self.block6(x5)
+        return x3, x4, x6
 
-    return pool3, pool4, encoder_model
 
-def decoder(
-    encoder_network, 
-    num_classes, 
-    model_type='fcn_8s'
-):
+class FCN32s(nn.Module):
+
+    def __init__(self, num_classes: int):
+        super(FCN8s, self).__init__()
+        self.encoder = Encoder()
+
+        self.upsample32x = nn.Upsample(scale_factor=32, mode='bilinear')
+        self.block = nn.Sequential(
+            nn.Conv2d(512, 256, kernel_size=1, stride=1, padding=0),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, num_classes, kernel_size=1, stride=1, padding=0),
+        )
+
+    def forward(self, x):
+        _, _, conv7 = self.encoder(x)
+        x = self.upsample32x(conv7)
+        x = self.block(x)
+        return x
+
+
+class FCN16s(nn.Module):
+
+    def __init__(self, num_classes: int):
+        super(FCN16s, self).__init__()
+        self.encoder = Encoder()
+
+        self.upsample2x = nn.Upsample(scale_factor=2, mode='bilinear')
+        self.upsample16x = nn.Upsample(scale_factor=16, mode='bilinear')
+
+        self.block = nn.Sequential(
+            nn.Conv2d(512+256, 256, kernel_size=1, stride=1, padding=0),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, num_classes, kernel_size=1, stride=1, padding=0),
+        )
+
+    def forward(self, x):
+        _, pool4, conv7 = self.encoder(x)
+        conv7x2 = self.upsample2x(conv7)
+        out = torch.cat([conv7x2, pool4], dim=1)
+        out = self.block(out)
+        out = self.upsample16x(out)
+        return out
+
+
+class FCN8s(nn.Module):
+
+    def __init__(self, num_classes: int):
+        super(FCN8s, self).__init__()
+        self.encoder = Encoder()
+
+        self.upsample2x = nn.Upsample(scale_factor=2, mode='bilinear')
+        self.upsample4x = nn.Upsample(scale_factor=4, mode='bilinear')
+        self.upsample8x = nn.Upsample(scale_factor=8, mode='bilinear')
+
+        self.block = nn.Sequential(
+            nn.Conv2d(256+512+512, 512, kernel_size=1, stride=1, padding=0),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 256, kernel_size=1, stride=1, padding=0),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, num_classes, kernel_size=1, stride=1, padding=0),
+        )
+
+    def forward(self, x):
+        pool3, pool4, conv7 = self.encoder(x)
+        conv7x4 = self.upsample4x(conv7)
+        pool4x2 = self.upsample2x(pool4)
+        out = torch.cat([conv7x4, pool4x2, pool3], dim=1)
+        out = self.block(out)
+        out = self.upsample8x(out)
+        return out
+
+
+def get_fcn(num_classes: int, model_type: str='fcn_32s'):
     assert model_type in ('fcn_32s', 'fcn_16s', 'fcn_8s'), \
-        'you should be select in fcn_32s or fcn_16s or fcn_8s'
+        f'The type {model_type} does not exists'
 
-    pool3, pool4, encoder = encoder_network
+    if model_type == 'fcn_32s':
+        model = FCN32s(num_classes)
 
-    if model_type=='fcn_32s':
-        upsample_x32 = UpSampling2D(size=(32,32), data_format='channels_last', interpolation='bilinear')(encoder.output)
-        conv_out = Conv2D(num_classes, kernel_size=3, strides=1, padding='same', activation='softmax')(upsample_x32)
-        model = Model(inputs=[encoder.input], outputs=[conv_out])
-
-    elif model_type=='fcn_16s':
-        conv7_x2 = UpSampling2D(size=(2,2), data_format='channels_last', interpolation='bilinear')(encoder.output)
-        pool4 = pool4
-        concat = concatenate([pool4, conv7_x2])
-        upsample_x16 = UpSampling2D(size=(16,16), data_format='channels_last', interpolation='bilinear')(concat)
-        conv_out = Conv2D(num_classes, kernel_size=3, strides=1, padding='same', activation='softmax')(upsample_x16)
-        model = Model(inputs=[encoder.input], outputs=[conv_out])
+    elif model_type == 'fcn_16s':
+        model = FCN16s(num_classes)
 
     else:
-        conv7_x4 = UpSampling2D(size=(4,4), data_format='channels_last', interpolation='bilinear')(encoder.output)
-        pool4_x2 = UpSampling2D(size=(2,2), data_format='channels_last', interpolation='bilinear')(pool4)
-        pool3 = pool3
-        concat = concatenate([conv7_x4, pool4_x2, pool3])
-        upsample_x8 = UpSampling2D(size=(8,8), data_format='channels_last', interpolation='bilinear')(concat)
-        conv_out = Conv2D(num_classes, kernel_size=3, strides=1, padding='same', activation='softmax')(upsample_x8)
-        model = Model(inputs=[encoder.input], outputs=[conv_out])
-    
+        model = FCN8s(num_classes)
+
     return model
-
-def get_fcn_32s(img_size, num_classes):
-    return decoder(encoder(img_size), num_classes, model_type='fcn_32s')
-
-def get_fcn_16s(img_size, num_classes):
-    return decoder(encoder(img_size), num_classes, model_type='fcn_16s')
-
-def get_fcn_8s(img_size, num_classes):
-    return decoder(encoder(img_size), num_classes, model_type='fcn_8s')
